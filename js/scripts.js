a07b428757498c769996f19c94c45ff8f9349b33
Marc Larkin and Sam Kirsch <marc@>
2017-01-12 11:57:49 -0800

    added the basic scripting to generate values

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+
+
 $(document).ready(function() {
-  
+  $("form.height").submit(function(event){
+    event.preventDefault();
+    var heightInput = $("#height").val();
+    if (heightInput === xsmall) {
+      $(".xsmall").parent().parent().removeClass("gray");
+      $(".overlay").hide();
+    }
+    else if (heightInput === short) {
+
+
+    }
+    else if (heightInput === medium) {
+
+    }
+    else if (heightInput === tall) {
+
+    }
+    else {
+
+    }
+  });
 });
